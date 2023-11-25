c8ae80f811045f3546928ab7753be0fdd86a990d
Cenny Wenner <cwenner@gmail.com>
2023-11-26 00:18:40 +0100

    Have no a-e or 1-5 selection by default

--- truthfulqa_evaluation.py
+++ truthfulqa_evaluation.py
@@ -48,7 +48,7 @@ def evaluate_truthfulqa_sample_mc1_on_chat_model(
     sample,
     model_name,
     select_a_to_e=False,
-    select_1_to_5=True,
+    select_1_to_5=False,
     verbose=False,
 ) -> float:
     """This evaluates a sample from the TruthfulQA dataset with an alternative representation due to API limitations."""

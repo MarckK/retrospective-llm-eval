e8820824c137585a2a7e8754f8a873c8db1d9c8c
Cenny Wenner <cwenner@gmail.com>
2023-11-26 13:44:11 +0100

    Add sample code to generate extensions of datasets

---
+++ generate_dataset.py
@@ -0,0 +1,55 @@
+import datasets
+import json
+import litellm
+import pandas as pd
+import tqdm
+
+# @TODO make deterministic
+
+
+def generate_similar_dataset_sample(
+    samples: datasets.Dataset,
+    model_name="gpt-3.5-turbo",
+    max_tokens=500,
+):
+    samples = samples.shuffle()
+    resp = litellm.completion(
+        model=model_name,
+        messages=[
+            {
+                "role": "user",
+                "content": "Continue this list:\n\n"
+                + "\n".join("* " + json.dumps(sample) for sample in samples),
+            }
+        ],
+        temperature=0.0,
+        max_tokens=max_tokens,
+        stop=["\n*"],
+    )
+    content = resp.choices[0].message.content.strip().split("\n\n")[0].replace("* ", "")
+    # @TODO find cleaner way to do this
+    return datasets.Dataset.from_pandas(
+        pd.DataFrame.from_records([json.loads(content)])
+    )
+
+
+def generate_similar_dataset(
+    dataset: datasets.Dataset,
+    target_size: int,
+    model_name="gpt-3.5-turbo",
+    examples_per_generation=5,
+    max_tokens_per_sample=500,
+):
+    generated_samples = []
+    for _ in tqdm.range(target_size):
+        generated_samples.append(
+            generate_similar_dataset_sample(
+                # Random selection each time
+                samples=dataset.select(range(len(dataset)))
+                .shuffle()
+                .select(range(examples_per_generation)),
+                model_name=model_name,
+                max_tokens=max_tokens_per_sample,
+            )
+        )
+    return datasets.concatenate_datasets(generated_samples)

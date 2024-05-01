61e903b10f66387221dc7fa4455408eb93c8dbe6
jacob-haimes <jacob.d.haimes@gmail.com>
2024-04-30 22:56:53 -0600

    add new datasets

--- convert_dataset.py
+++ convert_dataset.py
@@ -3,7 +3,9 @@ import numpy as np
 import os
 
 
-IN_CSV_LABEL = "jacob"
+IN_CSV_LABEL = "from-sheet_kunvar_jacob-edit_v1"
+OUT_CSV_LABEL = "kunvar_jacob-edit_v1"
+MAX_INCORRECT = 4
 
 def convert_exported_dataset():
     if not os.path.exists(f"data/datasets/{IN_CSV_LABEL}.csv"):
@@ -23,12 +25,12 @@ def convert_exported_dataset():
                             x["Correct"],
                     ] + [
                         x[f"Incorrect{i}"]
-                        for i in range(1, 11)
+                        for i in range(1, MAX_INCORRECT)
                     ]
                     if x
                 ],
                 labels=np.array(
-                    [1] + [0] * sum(bool(x[f"Incorrect{i}"]) for i in range(1, 11)),
+                    [1] + [0] * sum(bool(x[f"Incorrect{i}"]) for i in range(1, MAX_INCORRECT)),
                     dtype=np.int32
                 )
             ),
@@ -36,7 +38,7 @@ def convert_exported_dataset():
         remove_columns=in_ds.column_names
     )
 
-    in_ds.to_pandas().to_csv(f"data/datasets/crafted_{IN_CSV_LABEL}_new.csv")
+    in_ds.to_pandas().to_csv(f"data/datasets/{OUT_CSV_LABEL}_.csv")
 
 
 if __name__ == "__main__":
